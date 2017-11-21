b52957975168c8ec642f9f05c000ebc9c70ea2c7
Eduardo Lemos <eddu1930@gmail.com>
2017-11-20 22:24:34 -0500

    Update images.js

--- js/images.js
+++ js/images.js
@@ -1,1 +1,30 @@
 
+// attach event handlers with jquery
+$(document).ready(function () {
+  $("#next").on("click", function (e) {
+      next();
+  });
+
+  $("#prev").on("click", function (e) {
+      prev();
+  });
+
+  var images = new Array(
+      "../img/display/1.jpg",
+      "../img/display/2.jpg",
+      "../img/display/3.jpg");
+
+  function getCurrentImageIndex() {
+      return images.indexOf(document.getElementById("image").src);
+  }
+
+  function next() {
+      nextImage = (getCurrentImageIndex() + 1) % images.length;
+      document.getElementById("image").src = images[nextImage];
+  }
+
+  function prev() {
+      nextImage = (getCurrentImageIndex() - 1 + images.length) % images.length;
+      document.getElementById("image").src = images[nextImage];
+  }
+});

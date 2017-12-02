31e1ed1b704f89026fc86cef40d032f72e9b2b65
Eduardo Lemos <eddu1930@gmail.com>
2017-12-02 13:58:14 -0500

    Update maps.js

--- js/maps.js
+++ js/maps.js
@@ -1,8 +1,20 @@
 function initMap() {
+  // Map options
   var options = {
     zoom:11,
     center:{lat:42.9849, lng:-81.2453}
   }
 
+  // New map
   var map = new google.maps.Map(document.getElementById('map'), options);
+
+  // Add marker
+  addMarker({lat:42.9849, lng:-81.2453});
+
+  function addMarker(coords){
+    var marker = new google.maps.Marker({
+      position:coords,
+      map:map
+    });
+  }
 }
